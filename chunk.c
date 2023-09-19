dac907d41997b9fccb3a9b993018f2f7f8d18b7b
AkshayWarrier <aksmen121@gmail.com>
2023-09-19 11:31:40 +0530

    Add compiler

--- chunk.c
+++ chunk.c
@@ -3,35 +3,42 @@
 #include "chunk.h"
 #include "memory.h"
 
-void initChunk(Chunk* chunk) {
-    chunk->count = 0;
-    chunk->lines_ix = 0;
-    chunk->capacity = 0;
-    chunk->code = NULL;
-    chunk->lines = NULL;
-    initValueArray(&chunk->constants);
+void initChunk(Chunk *chunk)
+{
+  chunk->count = 0;
+  chunk->lines_ix = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+  chunk->lines = NULL;
+  initValueArray(&chunk->constants);
 }
 
-void freeChunk(Chunk* chunk) {
-    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    FREE_ARRAY(int, chunk->lines, chunk->capacity);
-    freeValueArray(&chunk->constants);
-    initChunk(chunk);
+void freeChunk(Chunk *chunk)
+{
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  FREE_ARRAY(int, chunk->lines, chunk->capacity);
+  freeValueArray(&chunk->constants);
+  initChunk(chunk);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte, int line) {
-  if (chunk->capacity < chunk->count + 1) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
+{
+  if (chunk->capacity < chunk->count + 1)
+  {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code,
-        oldCapacity, chunk->capacity);
+                             oldCapacity, chunk->capacity);
     chunk->lines = GROW_ARRAY(int, chunk->lines,
-        oldCapacity, chunk->capacity);
+                              oldCapacity, chunk->capacity);
   }
   chunk->code[chunk->count] = byte;
-  if(chunk->lines_ix > 0 && chunk->lines[chunk->lines_ix-1] == line) {
+  if (chunk->lines_ix > 0 && chunk->lines[chunk->lines_ix - 1] == line)
+  {
     chunk->lines[chunk->lines_ix]++;
-  } else {
+  }
+  else
+  {
     if (chunk->lines_ix > 0)
       chunk->lines_ix++;
     chunk->lines[chunk->lines_ix] = line;
@@ -41,17 +48,20 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   chunk->count++;
 }
 
-int getLine(Chunk* chunk, int offset) {
+int getLine(Chunk *chunk, int offset)
+{
   int ix = 1;
   int num = chunk->lines[ix];
-  while (offset+1 > num) {
+  while (offset + 1 > num)
+  {
     ix += 2;
     num += chunk->lines[ix];
   }
-  return chunk->lines[ix-1];
+  return chunk->lines[ix - 1];
 }
 
-int addConstant(Chunk* chunk, Value value) {
+int addConstant(Chunk *chunk, Value value)
+{
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
 }
--- memory.c
+++ memory.c
@@ -3,15 +3,18 @@
 
 #include "memory.h"
 
-void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
-    if (newSize == 0) {
+void *reallocate(void *pointer, size_t oldSize, size_t newSize)
+{
+    if (newSize == 0)
+    {
         free(pointer);
         return NULL;
     }
 
-    void* result = realloc(pointer, newSize);
+    void *result = realloc(pointer, newSize);
     // Initialize result to 0s
     memset(result + oldSize, 0, newSize - oldSize);
-    if (result == NULL) exit(1);
-    return result;    
+    if (result == NULL)
+        exit(1);
+    return result;
 }
